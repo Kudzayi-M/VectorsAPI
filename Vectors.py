8bf5ea310b4df3144c46d1a8d8cfede360371dcc
Kudzayi Mberi <44672827+Kudzayi-M@users.noreply.github.com>
2018-11-14 12:49:37 +0000

    Update Vectors.py

--- Vectors.py
+++ Vectors.py
@@ -123,7 +123,4 @@ class KMVector:
 			return "Error code: 0x000003"
 
 	def __str__(self):
-		return "Vector: <{}>\n Dimensions: {}\n Magnitude: {}\n Is resultant: {}\n Has Parents: {}\n".format(str(self.Vec)[1:-1]) 
-
-
-
+		return "Vector: <{}>\n Dimensions: {}\n Magnitude: {}\n Is resultant: {}\n Has Parents: {}\n".format((str(self.Vec)[1:-1]),len(self.Vec),"Unknown",self.isResultantVec,self.resultantParentVectors)
